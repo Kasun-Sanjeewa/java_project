038bacd70e2c6d062d41c80d04435bde094c6b44
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-03-24 22:01:57 +0530

    Add while loop

--- src/App.java
+++ src/App.java
@@ -3,6 +3,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // int x = 2;
+
+        // while (x <= 10) {
+        // System.out.println(x);
+        // x = x + 2;
+        // }
+
         // System.out.println("Hello, World!");
         // System.out.println("This is my first program");
 
