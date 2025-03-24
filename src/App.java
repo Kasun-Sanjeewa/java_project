9a08e3b5e2eb4c877fcb7f0103021b47ea90682c
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-03-24 21:38:00 +0530

    Ok

---
+++ src/App.java
@@ -0,0 +1,46 @@
+// import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first program");
+
+        // int a = 100;
+
+        // if (a != 10) {
+        // System.out.println("Youre correct");
+        // } else {
+        // System.out.println("Your are wrong");
+        // }
+        // int a, b, sum;
+        // a = 10;
+        // b = 8;
+        // sum = a % b;
+
+        // System.out.println(sum);
+
+        // Scanner x = new Scanner(System.in);
+
+        // System.out.print("Enter yout name: ");
+        // String name = x.nextLine();
+        // System.out.println(name);
+
+        // System.out.print("Enter your number: ");
+        // int num = x.nextInt();
+        // System.out.print(num);
+
+        // x.close();
+
+        // int a = 10;
+        // char b = 'A';
+        // double c = 5.5;
+        // String d = "This is sting veriable";
+        // boolean e = true;
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+        // System.out.println(e);
+    }
+}
