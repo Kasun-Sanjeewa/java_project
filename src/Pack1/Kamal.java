b131f697dd8c3373cffd25fcbda5387a58c5d8a7
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-03-26 19:37:51 +0530

    add sccess modyfier

---
+++ src/Pack1/Kamal.java
@@ -0,0 +1,11 @@
+package Pack1;
+
+import Pack2.Saman;
+
+public class Kamal {
+    public static void main(String[] args) {
+        Kamal obj1 = new Kamal();
+        Nimal obj2 = new Nimal();
+        Saman onj3 = new Saman(); // Cant access, because this is a default class
+    }
+}
