773221e40b173190b8685a2b837f77e2f3389e3f
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-03-27 00:09:32 +0530

    Add encapsulation

---
+++ src/Encapsulation2.java
@@ -0,0 +1,11 @@
+public class Encapsulation2 {
+    public static void main(String[] args) {
+        Encapsulation obj1 = new Encapsulation();
+        obj1.setName("Kasun");
+        System.out.println(obj1.getName());
+
+        obj1.setAge(24);
+        System.out.println(obj1.getAge());
+
+    }
+}
